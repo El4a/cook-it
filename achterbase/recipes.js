a80a82b6c239929c865889b101e6dcbe0900e69a
El4a <elfiera.p@gmail.com>
2018-11-24 15:45:03 +0100

    First working lambda p2

--- achterbase/recipes.js
+++ achterbase/recipes.js
@@ -1,9 +1,22 @@
 // eslint-disable-next-line import/prefer-default-export
+import AWS from 'aws-sdk';
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 export const get = async () => {
 
-  return {
-    statusCode: 200,
-    body: "Charlie bit ma finger"
-  };
-  
+  const params = {TableName: process.env.RECIPES_TABLE};
+
+  try {
+    let result = await dynamoDb.scan(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.Items)
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: "Did someone drop our table?? " + e
+    };
+  } 
 };
