750036916c886ae2e197354a4a17a6e8a00158eb
El4a <elfiera.p@gmail.com>
2018-12-26 14:56:05 +0100

    Add fontawesome icons

--- frokant/src/App.vue
+++ frokant/src/App.vue
@@ -15,6 +15,12 @@ export default {
   components: {
     Navigation,
     HelloWorld
+  },
+  head: {
+    link: [
+      { rel:'stylesheet', href:'https://use.fontawesome.com/releases/v5.6.3/css/solid.css', integrity:'sha384-+0VIRx+yz1WBcCTXBkVQYIBVNEFH1eP6Zknm16roZCyeNg2maWEpk/l/KsyFKs7G', crossorigin:"anonymous"},
+      { rel:"stylesheet", href:"https://use.fontawesome.com/releases/v5.6.3/css/fontawesome.css" ,integrity:"sha384-jLuaxTTBR42U2qJ/pm4JRouHkEDHkVqH0T1nyQXn1mZ7Snycpf6Rl25VBNthU4z0", crossorigin:"anonymous" }
+    ]
   }
 }
 </script>
--- frokant/src/main.js
+++ frokant/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import VueHead from 'vue-head'
 import App from './App.vue'
 
+Vue.use(VueHead)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
