4b62ac75e58e9abe7ce320ab00a96e1e155f20e7
El4a <elfiera.p@gmail.com>
2018-12-26 14:10:01 +0100

    Add navbar

--- frokant/src/App.vue
+++ frokant/src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img class="banner" alt="banner" src="./assets/banner.png">
+    <Navigation/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/navigation.vue'
 
 export default {
   name: 'app',
   components: {
+    Navigation,
     HelloWorld
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+
+body {
+  margin: 0;
+  background-color: rgba(226,219,219,.5);
+}
+
+.banner {
+  width: 100%;
 }
 </style>
---
+++ frokant/src/components/navigation.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav role="navigation">
+    <ul>
+      <li>
+        <a href="javscript:void()" class="active">Public</a>
+      </li>
+      <li>
+        <a href="javscript:void()">Mine</a>
+      </li>
+      <li>
+        <a href="javscript:void()">Favorites</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  //name: 'navigation', //why do you need a name here - works without
+  props: {
+    
+  }
+}
+</script>
+
+
+<style lang="scss">
+@import "../assets/vars.scss";
+
+nav {
+  margin-top: -3.5rem;
+  text-align: center;
+  font-size: 1.3rem;
+  font-family: Helvetica;
+  ul {
+    list-style: none;
+    li {
+      display: inline-block;
+      background-color: $color-dark;
+      padding: 1rem 0;
+
+      &:first-child {
+        padding-left: 1.5rem;
+      }
+      &:last-child {
+        padding-right: 1.5rem;
+      }
+
+      a {
+        padding: 1rem 1.5rem; 
+        text-decoration: none;
+        color: #fff;
+
+        &.active {
+          background-color: $color-theme;
+        }
+
+        &:not(.active):hover {
+          color: $color-theme;
+        }
+      }
+    }
+  }
+}
+</style>
