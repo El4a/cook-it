1ae6bbed685ae99d952f4a61e7998a5fa24de3a0
El4a <elfiera.p@gmail.com>
2019-01-01 19:04:16 +0100

    Install vue router

--- frokant/src/main.js
+++ frokant/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueHead from 'vue-head'
 import App from './App.vue'
+import router from './services/RouterService'
 
 Vue.use(VueHead)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router: router,
 }).$mount('#app')
 
